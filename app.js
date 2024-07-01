ce5deea236ce9f9fc78e45725f63eca2f4733c24
João Oliveira <joaofilho28@hotmail.com>
2024-07-01 07:44:00 -0300

    Update app.js
    Ajustes nos comentários dentro do código.

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-/*
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-*/
-
-// cira uma lista vazia 
+// cria as variáveis com os parâmetros necessários para o jogo
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
-
-// declação das variáveis
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-
-// declaração das funções
-
-// função exibir textos na tela
+// função que exibirá os textos informativos na tela do jogo e inicia a framework de voz
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
-// função exibir mensagem inicial
+// função que exibe mensagem inicial na tela do jogo
 function exibirMensagemInicial () {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
     exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
-// função verificar chute
+// função que verifica as tentativas de chute do jogador
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
@@ -50,7 +38,7 @@ function verificarChute() {
     }
 }
 
-// função que gera o número aletório
+// função que gera o número aletório entre 1 e 10, além de armazenar em uma lista vazia parq que o número não se repita
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1 );
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
@@ -67,13 +55,13 @@ function gerarNumeroAleatorio() {
     }
 }
 
-// função limpa campo input
+// função que limpa o campo de informação do número a ser 'chutado' pelo jogador
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';    
 }
 
-// função reinicia o Jogo
+// função que reinicia o Jogo após o número escolhido aleatoriamente ser acertado
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
@@ -82,5 +70,5 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
 
-// execução da função inicial
+// execução da função que exibe as mensagens iniciais do Jogo do Número Secreto
 exibirMensagemInicial();
